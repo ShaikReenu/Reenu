cffa2088e6a8c69d338a0325a3e7e61cf9098d4d
ShaikReenu <47174310+ShaikReenu@users.noreply.github.com>
2019-04-22 22:45:14 +0530

    Add files via upload

---
+++ linr.py
@@ -0,0 +1,19 @@
+import numpy as nm
+import cmath as m
+def dft(x) :
+ X1=input("enter the samples:")
+ l1=len(X1)
+ for k in range(l1):
+ 	s=0
+ 	for i in range(l1):
+ 		s +=x[n]*m.exp(-j*2*nm.pi*k*n/l1)
+ 		X.append(s)
+ X2=input("enter the samples:")
+ l2=len(X2)
+ for k in range(l2):
+ 	s=0
+ 	for i in range(l2):
+ 		s +=x[n]*m.exp(-j*2*nm.pi*k*n/l2)
+ 		X.append(s)
+ 
+
